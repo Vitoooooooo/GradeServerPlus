9290bf1d5cfb6b8283d3d519ee295b57e601ffb8
yufangfred <iam@vitochen.net>
2017-05-03 19:03:11 -0400

    todos

--- Controller/login.js
+++ Controller/login.js
@@ -1,7 +1,7 @@
 function userPasswordValidation() {
     var username = document.forms["loginInfo"]["username"].value;
     var password = document.forms["loginInfo"]["password"].value;
-          
+    //todo @vito chen update this after jerry created users table.
     if (username === "admin" && password === "123") {
         return true;
     }
